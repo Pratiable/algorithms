2c6e3e0f125b5855c8dc6b4ee0a1dabc5ff41539
Pratiable <ambitiouskyle@gmail.com>
2022-08-19 12:48:45 +0900

    feat(BOJ): solved 1547

---
+++ BOJ/1547.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        int n, m;
+        int now = 1;
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            now = now == n ? m : now == m ? n : now;
+        }
+        System.out.println(now);
+    }
+}
