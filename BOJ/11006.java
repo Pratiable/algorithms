798992ac4d15707a3927e3abc62c13c2fd7b4ee5
Pratiable <ambitiouskyle@gmail.com>
2022-08-19 11:50:09 +0900

    feat(BOJ): solved 11006

---
+++ BOJ/11006.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        int n, m;
+
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+
+            bw.append(String.format("%d %d\n", m - (n - m), n - m));
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
