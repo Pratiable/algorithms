a01c11d9280d1fc5d00ac2f1151ff96dfd183913
Pratiable <ambitiouskyle@gmail.com>
2022-09-03 12:33:40 +0900

    feat(BOJ): solved 5532

---
+++ BOJ/5532.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+
+        do {
+            l--;
+            a -= c;
+            b -= d;
+        } while (a > 0 || b > 0);
+
+        System.out.println(l);
+    }
+}
