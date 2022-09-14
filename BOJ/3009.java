ec3e777d38c97d402c15bcf3d039246ed299833d
Pratiable <ambitiouskyle@gmail.com>
2022-09-14 09:22:19 +0900

    feat(BOJ): solved 3009

---
+++ BOJ/3009.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int x3 = Integer.parseInt(st.nextToken());
+        int y3 = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d %d", x1 == x2 ? x3 : x1 == x3 ? x2 : x1, y1 == y2 ? y3 : y1 == y3 ? y2 : y1);
+    }
+}
