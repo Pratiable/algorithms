bd57075bf07179caa3f694ea13a4949c3e16b679
Pratiable <ambitiouskyle@gmail.com>
2022-09-20 14:45:30 +0900

    feat(BOJ): solved 2455

---
+++ BOJ/2455.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int max = 0;
+        int now = 0;
+
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            int out = Integer.parseInt(st.nextToken());
+            int in = Integer.parseInt(st.nextToken());
+
+            now -= out;
+            now += in;
+            max = Math.max(max, now);
+        }
+        System.out.println(max);
+    }
+}
