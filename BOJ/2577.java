1e58ad2d4d17198e0ee841b3a3a6d81fcaff6a54
Pratiable <ambitiouskyle@gmail.com>
2022-09-21 00:50:20 +0900

    feat(BOJ): solved 2577

---
+++ BOJ/2577.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        String result = String.valueOf(a * b * c);
+
+        for (int i = 0; i < 10; i++) {
+            bw.append(String.valueOf(countChar(result, Character.forDigit(i, 10)))).append("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int countChar(String str, char ch) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
