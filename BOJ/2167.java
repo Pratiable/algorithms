ab344a76c6725549618d8fa12ef2281e1c417560
Pratiable <ambitiouskyle@gmail.com>
2022-10-09 19:00:39 +0900

    feat(BOJ): solved 2167

---
+++ BOJ/2167.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int l = 0; l < k; l++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int sum = 0;
+            for (int a = i; a <= x; a++) {
+                for (int b = j; b <= y; b++) {
+                    sum += map[a][b];
+                }
+            }
+            bw.append(String.valueOf(sum)).append("\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
