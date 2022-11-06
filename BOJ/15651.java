0f72591c9b18a96b91acf96ccb861d7696450fa9
Pratiable <ambitiouskyle@gmail.com>
2022-11-06 21:42:44 +0900

    feat(BOJ): solved 15651

---
+++ BOJ/15651.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static int n;
+    public static int m;
+    public static int[] num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[m + 1];
+        make(0);
+
+        System.out.println(sb);
+    }
+
+    public static void make(int count) {
+        if (count == m) {
+            for (int i = 1; i <= m; i++) {
+                sb.append(num[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int j = 1; j <= n; j++) {
+            num[count + 1] = j;
+            make(count + 1);
+        }
+    }
+}
