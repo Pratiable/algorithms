1d47c2e34bcc0d0f536d56508d854749cb95d6ad
Pratiable <ambitiouskyle@gmail.com>
2022-11-18 09:51:05 +0900

    feat(BOJ): solved 2217

---
+++ BOJ/2217.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int max = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int j = 0; j < arr.length; j++) {
+            max = Math.max(max, arr[j] * (arr.length - j));
+        }
+
+        System.out.println(max);
+    }
+}
