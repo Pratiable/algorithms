879a3e1a59b808cc2fb2a2d6f8ed6438f88ae45e
Pratiable <ambitiouskyle@gmail.com>
2022-11-19 22:03:22 +0900

    feat(BOJ): solved 5337

---
+++ BOJ/5337.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
