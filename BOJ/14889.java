cf8a6d43cb689e546861c2150e4ac5c3adbc2ca8
Pratiable <ambitiouskyle@gmail.com>
2022-12-07 21:22:53 +0900

    feat(BOJ): solved 14889

---
+++ BOJ/14889.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, min;
+    static boolean[] visited;
+    static int[][] abilityMap;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        min = Integer.MAX_VALUE;
+        visited = new boolean[n];
+        abilityMap = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                abilityMap[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        makeTeam(0, 0);
+        System.out.println(min);
+    }
+
+    static void makeTeam(int depth, int idx) {
+        if (depth == n / 2) {
+            calculateScore();
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            makeTeam(depth + 1, i + 1);
+            visited[i] = false;
+        }
+    }
+
+    static void calculateScore() {
+        int startScore = 0;
+        int linkScore = 0;
+        for (int i = 0; i < n; i++) {
+            boolean isStart = visited[i];
+            for (int j = 0; j < n; j++) {
+                if (isStart && visited[j]) {
+                    startScore += abilityMap[i][j];
+                } else if (!isStart && !visited[j]) {
+                    linkScore += abilityMap[i][j];
+                }
+            }
+        }
+
+        min = Math.min(min, Math.abs(startScore - linkScore));
+    }
+}
