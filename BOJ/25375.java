b506aa3d7f7ca151c643a7e79ace6d532a0b2a82
Pratiable <ambitiouskyle@gmail.com>
2022-12-25 15:40:39 +0900

    feat(BOJ): solved 25375

---
+++ BOJ/25375.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int Q = Integer.parseInt(br.readLine());
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            sb.append(a * 2 <= b && b % a == 0 ? 1 : 0).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
