acfaf1848d42e112f98978e1537eb236ac114b12
Pratiable <ambitiouskyle@gmail.com>
2023-01-25 23:03:59 +0900

    feat(BOJ): solved 1271

---
+++ BOJ/1271.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        System.out.printf(n.divide(m) + "\n" + n.remainder(m));
+    }
+}
