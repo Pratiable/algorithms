30e4a25375fa33d1747e859a7dd74090d5345c04
Pratiable <ambitiouskyle@gmail.com>
2023-03-25 23:53:09 +0900

    feat(BOJ): solved 21300

---
+++ BOJ/21300.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		int sum = a + b + c + d + e + f;
+        
+		System.out.println(sum * 5);
+	}
+}
