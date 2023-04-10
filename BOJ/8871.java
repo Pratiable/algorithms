3a6832a1ebf4d796e62f9a8c0faad5af8c6a6a97
Pratiable <ambitiouskyle@gmail.com>
2023-04-10 20:17:15 +0900

    feat(BOJ): solved 8871

---
+++ BOJ/8871.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()) + 1;
+        System.out.printf("%d %d", n * 2, n * 3);
+    }
+}
