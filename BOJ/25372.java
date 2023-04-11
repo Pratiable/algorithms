44410fbbc9608b256b46b37c1927c196494b3e7a
Pratiable <ambitiouskyle@gmail.com>
2023-04-11 17:22:31 +0900

    feat(BOJ): solved 25372

---
+++ BOJ/25372.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			String str = br.readLine().trim();
+			sb.append(str.length() >= 6 && str.length() <= 9 ? "yes\n" : "no\n");
+		}
+        System.out.println(sb);
+	}
+}
