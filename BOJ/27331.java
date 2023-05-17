3e57f17f8cf950bebf8373e9b5b0e10e9186c975
Pratiable <ambitiouskyle@gmail.com>
2023-05-17 17:19:34 +0900

    feat(BOJ): solved 27331

---
+++ BOJ/27331.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        System.out.println(a + b);
+    }
+}
