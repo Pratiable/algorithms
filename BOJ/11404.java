462d20e73db75147e5de02f33878c6990016f127
Pratiable <ambitiouskyle@gmail.com>
2023-05-28 21:44:23 +0900

    feat(BOJ): solved 11404

---
+++ BOJ/11404.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        long INF = 100_000_000_000_000L;
+        long[][] dist = new long[n + 1][n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                    continue;
+                }
+                dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (dist[a][b] <= c) continue;
+            dist[a][b] = c;
+        }
+
+        for (int k = 1; k < n + 1; k++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
