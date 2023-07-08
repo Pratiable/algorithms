815e5eb43fc21d6bd2e531f7d9a03234a0973c47
Pratiable <ambitiouskyle@gmail.com>
2023-07-08 23:01:02 +0900

    feat(BOJ): solved 24072

---
+++ BOJ/24072.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (A <= C && C < B) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
