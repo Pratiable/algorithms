bc2fe49f152b15d91db735dadee085458e895ec9
Pratiable <ambitiouskyle@gmail.com>
2023-07-23 22:28:34 +0900

    feat(BOJ): solved 25784

---
+++ BOJ/25784.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[3];
+        nums[0] = Integer.parseInt(st.nextToken());
+        nums[1] = Integer.parseInt(st.nextToken());
+        nums[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(nums);
+
+        if (nums[2] == nums[0] + nums[1] || nums[2] == nums[0] * nums[1]) {
+            System.out.println(nums[2] == nums[0] + nums[1] ? 1 : 2);
+        } else {
+            System.out.println(3);
+        }
+    }
+}
