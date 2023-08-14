c525b5f6adafa6d339f7d65b5665393e2e6dd9f2
Pratiable <ambitiouskyle@gmail.com>
2023-08-14 23:40:44 +0900

    feat(BOJ): solved 25494

---
+++ BOJ/25494.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int count = 0;
+            for (int x = 1; x <= a; x++) {
+                for (int y = 1; y <= b; y++) {
+                    for (int z = 1; z <= c; z++) {
+                        if ((x % y) == (y % z) && (y % z) == (z % x)) {
+                            count++;
+                        }
+                    }
+                }
+            }
+            sb.append(count).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
