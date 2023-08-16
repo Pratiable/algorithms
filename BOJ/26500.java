7b72c174400c1a71c5cca221a734130c761261ba
Pratiable <ambitiouskyle@gmail.com>
2023-08-16 13:57:35 +0900

    feat(BOJ): solved 26500

---
+++ BOJ/26500.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double a = Double.parseDouble(st.nextToken());
+            double b = Double.parseDouble(st.nextToken());
+
+            double result = Math.abs(a - b);
+
+            sb.append(String.format("%.1f", result)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
