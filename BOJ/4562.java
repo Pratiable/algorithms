d31362ae7fa9da01fefc57f66dfd59cc61ccc928
Pratiable <ambitiouskyle@gmail.com>
2023-09-18 21:26:19 +0900

    feat(BOJ): solved 4562

---
+++ BOJ/4562.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a >= b) {
+                sb.append("MMM BRAINS\n");
+            } else {
+                sb.append("NO BRAINS\n");
+            }
+        }
+        
+        System.out.println(sb);
+    }
+}
