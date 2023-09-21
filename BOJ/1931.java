f6c5043b0dd2c8a61c9341b61093037c7d4a518a
Pratiable <ambitiouskyle@gmail.com>
2023-09-21 17:33:26 +0900

    feat(BOJ): solved 1931

---
+++ BOJ/1931.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] time = new int[n][2];
+
+        StringTokenizer st;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+
+            return o1[1] - o2[1];
+        });
+
+        int cnt = 0;
+        int last = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (last <= time[i][0]) {
+                last = time[i][1];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
