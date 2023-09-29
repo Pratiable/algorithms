2a28bb85073e3e2f1145ae9c379b265c14f539a9
Pratiable <ambitiouskyle@gmail.com>
2023-09-29 20:11:33 +0900

    feat(BOJ): solved 17219

---
+++ BOJ/17219.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            sb.append(map.get(br.readLine())).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
