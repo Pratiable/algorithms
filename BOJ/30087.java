a677584a8124e8635337c30629093cc1872bbeeb
Pratiable <ambitiouskyle@gmail.com>
2023-10-04 20:55:52 +0900

    feat(BOJ): solved 30087

---
+++ BOJ/30087.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Algorithm", "204");
+        map.put("DataAnalysis", "207");
+        map.put("ArtificialIntelligence", "302");
+        map.put("CyberSecurity", "B101");
+        map.put("Network", "303");
+        map.put("Startup", "501");
+        map.put("TestStrategy", "105");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            sb.append(map.get(str)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
