293b852d2212116315725aa1e95a4837ead0e714
Pratiable <ambitiouskyle@gmail.com>
2023-10-06 00:50:51 +0900

    feat(BOJ): solved 29863

---
+++ BOJ/29863.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int result = a > b ? b + 24 - a : b - a;
+
+        System.out.println(result);
+    }
+}
