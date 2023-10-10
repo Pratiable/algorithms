abc6458a282a6f425c6b5838bf6de0a5e2febade
Pratiable <ambitiouskyle@gmail.com>
2023-10-10 23:40:33 +0900

    feat(BOJ): solved 30328

---
+++ BOJ/30328.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) * 4000);
+    }
+}
