95588fa72e9acd611a158de23c0114eeb652c8f1
Pratiable <ambitiouskyle@gmail.com>
2023-10-16 22:04:37 +0900

    feat(BOJ): solved 25600

---
+++ BOJ/25600.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int s = a * (d + g);
+
+            max = Math.max(max, a == d + g ? s * 2 : s);
+        }
+
+        System.out.println(max);
+    }
+}
