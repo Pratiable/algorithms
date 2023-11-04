ddaa05b8a366821ee2b84ebcace0ec87bcd58236
Pratiable <ambitiouskyle@gmail.com>
2023-11-04 11:24:26 +0900

    feat(BOJ): solved 25625

---
+++ BOJ/25625.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        if (x > y) {
+            System.out.println(x + y);
+        } else {
+            System.out.println(y % x);
+        }
+    }
+}
