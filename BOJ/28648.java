4a8b1bcc6d9e3107c51dd44e76b8cc7a07eef363
Pratiable <ambitiouskyle@gmail.com>
2023-11-21 21:16:49 +0900

    feat(BOJ): solved 28648

---
+++ BOJ/28648.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            min = Math.min(min, a + b);
+        }
+
+        System.out.println(min);
+    }
+}
