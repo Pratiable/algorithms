91c104b518224dfd5055be31128bd4d0ebbac4fd
Pratiable <ambitiouskyle@gmail.com>
2023-11-30 22:42:12 +0900

    feat(BOJ): solved 21335

---
+++ BOJ/21335.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long a = Long.parseLong(br.readLine());
+        System.out.println(2 * Math.sqrt(Math.PI * a));
+    }
+}
