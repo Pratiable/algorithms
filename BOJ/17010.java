55a48e4475b5d089b0670501803367598832e4bb
Pratiable <ambitiouskyle@gmail.com>
2023-12-26 21:29:22 +0900

    feat(BOJ): solved 17010

---
+++ BOJ/17010.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+
+            sb.append(str.repeat(n)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
