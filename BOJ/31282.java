efd0ff62b4b8ba8ebad43803c6a05d8ffd982f17
Pratiable <ambitiouskyle@gmail.com>
2024-02-01 22:55:37 +0900

    feat(BOJ): solved 31282

---
+++ BOJ/31282.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int r = 0;
+        while (n > 0) {
+            n -= k - m;
+            r++;
+        }
+        
+        System.out.println(r);
+    }
+}
