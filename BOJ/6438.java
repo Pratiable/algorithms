c42ccf743cd9d5eb71eeeae25852d3961b5b1945
Pratiable <ambitiouskyle@gmail.com>
2024-03-07 19:34:12 +0900

    feat(BOJ): solved 6438

---
+++ BOJ/6438.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            StringBuffer sbf = new StringBuffer(br.readLine());
+            sb.append(sbf.reverse()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
