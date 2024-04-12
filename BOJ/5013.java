6eb30a8ce3da0172dc2283140aa861e2db9f7ae1
Pratiable <ambitiouskyle@gmail.com>
2024-04-12 15:35:30 +0900

    feat(BOJ): solved 5013

---
+++ BOJ/5013.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (br.readLine().contains("CD")) continue;
+            sum++;
+        }
+
+        System.out.println(sum);
+    }
+}
