2ae1b2a514386e4df4069700b4b45c9167bbc3a5
Pratiable <ambitiouskyle@gmail.com>
2024-04-28 01:34:25 +0900

    feat(BOJ): solved 14656

---
+++ BOJ/14656.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
