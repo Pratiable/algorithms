1031ae47462153184589eff68d0af071752c67f1
Pratiable <ambitiouskyle@gmail.com>
2024-05-20 15:31:21 +0900

    feat(BOJ): solved 23806

---
+++ BOJ/23806.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append("@".repeat(n * 5)).append("\n");
+        }
+        for (int i = 0; i < n * 3; i++) {
+            sb.append("@".repeat(n)).append(" ".repeat(n * 3)).append("@".repeat(n)).append("\n");
+        }
+        for (int i = 0; i < n; i++) {
+            sb.append("@".repeat(n * 5)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
