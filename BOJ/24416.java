68938f28ec7dbc859408598ffebf7333c8b705ab
Pratiable <ambitiouskyle@gmail.com>
2024-10-31 23:13:54 +0900

    feat(BOJ): solved 24416

---
+++ BOJ/24416.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] f = new long[n + 2];
+        f[1] = 1;
+        f[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        System.out.println(f[n] + " " + (n - 2));
+    }
+}
