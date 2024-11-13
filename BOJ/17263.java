8c2d31162e3d0bb2100a335decca41957b78337f
Pratiable <ambitiouskyle@gmail.com>
2024-11-14 00:32:47 +0900

    feat(BOJ): solved 17263

---
+++ BOJ/17263.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int max = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, Integer.parseInt(st.nextToken()));
+        }
+        
+        System.out.println(max);
+    }
+}
