9c510fa31e76449c2f31c58cf3f1a7f5f1d12786
Pratiable <ambitiouskyle@gmail.com>
2024-12-06 21:54:34 +0900

    feat(BOJ): solved 13528

---
+++ BOJ/13528.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double c = Double.parseDouble(br.readLine().trim());
+        int l = Integer.parseInt(br.readLine().trim());
+        double ans = 0;
+
+        for (int i = 0; i < l; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double a = Double.parseDouble(st.nextToken());
+            double b = Double.parseDouble(st.nextToken());
+            ans += a * b * c;
+        }
+
+        System.out.printf("%.7f", ans);
+    }
+}
