3d49c0bc685ede36fdc111556d4a43cfa8087aab
Pratiable <ambitiouskyle@gmail.com>
2024-12-24 16:11:08 +0900

    feat(BOJ): solved 26752

---
+++ BOJ/26752.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken()) + 1;
+
+        if (s == 60) {
+            s = 0;
+            m++;
+            if (m == 60) {
+                m = 0;
+                h++;
+                if (h == 24) {
+                    h = 0;
+                }
+            }
+        }
+
+        System.out.printf("%02d:%02d:%02d\n",h, m, s);
+    }
+}
