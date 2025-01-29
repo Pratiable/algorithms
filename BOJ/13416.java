d7f0d0861a71af171a71c12985d496e7763f325f
Pratiable <ambitiouskyle@gmail.com>
2025-01-30 01:49:02 +0900

    feat(BOJ): solved 13416

---
+++ BOJ/13416.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int max = 0;
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+
+                max += Math.max(Math.max(a, Math.max(b, c)), 0);
+            }
+            
+            sb.append(max).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
