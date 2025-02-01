7440c79643b7ca09e3c0c617de56473d0c7256c1
Pratiable <ambitiouskyle@gmail.com>
2025-02-02 03:00:20 +0900

    feat(BOJ): solved 14264

---
+++ BOJ/14264.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((Math.sqrt(3) / 4) * (Math.pow(n, 2)));
+    }
+}
