a3b23228d5f0a734b61d3e0937cfcc23e845a51f
Pratiable <ambitiouskyle@gmail.com>
2025-03-05 22:54:21 +0900

    feat(BOJ): solved 4108

---
+++ BOJ/4108.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if (n == 0 && m == 0) break;
+
+            char[][] map = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                String line = br.readLine();
+                for (int j = 0; j < m; j++) {
+                    map[i][j] = line.charAt(j);
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (map[i][j] == '*') continue;
+                    int cnt = 0;
+                    for (int k = 0; k < 8; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                        if (map[nx][ny] == '*') cnt++;
+                    }
+                    map[i][j] = (char) (cnt + '0');
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    sb.append(map[i][j]);
+                }
+                sb.append("\n");
+            }
+
+        }
+
+        System.out.println(sb);
+    }
+}
