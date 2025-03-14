84de47231ed59daf4a6ce7b9bc36644060ca4df2
Pratiable <ambitiouskyle@gmail.com>
2025-03-14 20:55:30 +0900

    feat(BOJ): solved 9655

---
+++ BOJ/9655.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n % 2 == 0 ? "CY" : "SK");
+    }
+}
