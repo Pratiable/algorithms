b694e0ab9684560634742efab981f4d394b7310d
Pratiable <ambitiouskyle@gmail.com>
2025-04-16 23:19:19 +0900

    feat(BOJ): solved 9506

---
+++ BOJ/9506.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == -1) break;
+            queue.clear();
+            int sum = 0;
+            for (int i = 1; i <= n / 2; i++) {
+                if (n % i != 0) continue;
+                queue.add(i);
+                sum += i;
+            }
+
+            if (sum != n) {
+                sb.append(n).append(" is NOT perfect.\n");
+                continue;
+            }
+
+            sb.append(n).append(" = ").append(queue.poll());
+            while (!queue.isEmpty()) {
+                sb.append(" + ").append(queue.poll());
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
