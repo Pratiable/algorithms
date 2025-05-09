adc1c8300b4fe8215fe9b185b18269b24b2d00a1
Pratiable <ambitiouskyle@gmail.com>
2025-05-10 01:13:21 +0900

    feat(BOJ): solved 4690

---
+++ BOJ/4690.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int a = 2; a <= 100; a++) {
+            for (int b = 2; b <= a; b++) {
+                for (int c = b; c <= a; c++) {
+                    for (int d = c; d <= a; d++) {
+                        if (Math.pow(a, 3) == Math.pow(b, 3) + Math.pow(c, 3) + Math.pow(d, 3)) {
+                            sb.append("Cube = ").append(a).append(", Triple = (").append(b).append(",").append(c).append(",").append(d).append(")\n");
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
