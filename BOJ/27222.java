ce10b890a41bd77f3918ab35466502cd1b1b0021
Pratiable <ambitiouskyle@gmail.com>
2025-06-01 04:25:19 +0900

    feat(BOJ): solved 27222

---
+++ BOJ/27222.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (nums[i] == 1 && x < y) {
+                sum += y - x;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
