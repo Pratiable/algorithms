d4fbbf25efa715031f237a150194d68d9e35b049
Pratiable <ambitiouskyle@gmail.com>
2025-10-16 03:32:00 +0900

    feat(BOJ): solved 16435

---
+++ BOJ/16435.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < N; i++) {
+            if (nums[i] > L) break;
+            L++;
+        }
+        System.out.println(L);
+    }
+}
