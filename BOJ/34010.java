c2451069fd5908defe4afdd921f01168dbaa3de1
Pratiable <ambitiouskyle@gmail.com>
2025-11-14 05:52:11 +0900

    feat(BOJ): solved 34010

---
+++ BOJ/34010.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println((2 * N - 2) + " " + (2 * N - 3));
+    }
+}
