3702e1e64302b56cd6b77a64243ab5ded75eeabd
Pratiable <ambitiouskyle@gmail.com>
2025-11-24 22:04:59 +0900

    feat(BOJ): solved 10821

---
+++ BOJ/10821.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+        int cnt = 0;
+
+        while (st.hasMoreTokens()) {
+            st.nextToken();
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
