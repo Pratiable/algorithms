9ad1290efc2a51503679f297924cdd9a1e1db67d
Pratiable <ambitiouskyle@gmail.com>
2025-12-10 06:11:30 +0900

    feat(BOJ): solved 10864

---
+++ BOJ/10864.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] friends = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            friends[a]++;
+            friends[b]++;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(friends[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
