5a269fb1c134209d189afb679cb5b61727467d64
Pratiable <ambitiouskyle@gmail.com>
2025-12-17 23:44:41 +0900

    feat(BOJ): solved 34529

---
+++ BOJ/34529.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int Z = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int U = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        System.out.print((U / 100) * X + (V / 50) * Y + (W / 20) * Z);
+    }
+}
