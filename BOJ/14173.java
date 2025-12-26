a445bda5354d7a0b02d13bd9995393431fd8c0c6
Pratiable <ambitiouskyle@gmail.com>
2025-12-26 23:50:57 +0900

    feat(BOJ): solved 14173

---
+++ BOJ/14173.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x3 = Integer.parseInt(st.nextToken());
+        int y3 = Integer.parseInt(st.nextToken());
+        int x4 = Integer.parseInt(st.nextToken());
+        int y4 = Integer.parseInt(st.nextToken());
+
+        int cal = Math.max(Math.max(x2, x4) - Math.min(x1, x3), Math.max(y2, y4) - Math.min(y1, y3));
+
+        System.out.print((int) Math.pow(cal, 2));
+    }
+}
