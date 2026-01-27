77fcff759222b5a8cc7fa8ab5d9451f1ccee08e3
Pratiable <ambitiouskyle@gmail.com>
2026-01-28 05:15:53 +0900

    feat(BOJ): solved 11094

---
+++ BOJ/11094.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            if (str.startsWith("Simon says")) {
+                sb.append(str.substring(10)).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
