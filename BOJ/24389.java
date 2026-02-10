a3184824e0430a7affb1a757cad67d9d8f3ab20d
Pratiable <ambitiouskyle@gmail.com>
2026-02-10 22:59:28 +0900

    feat(BOJ): solved 24389

---
+++ BOJ/24389.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.print(Integer.bitCount(N ^ (~N + 1)));
+    }
+}
